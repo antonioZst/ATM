0485fce85e55313a2797fce4713b42f60305959a
antonioZst <antonio.moreno.abundis@gmail.com>
2025-05-09 08:26:17 -0600

    struct cliente

---
+++ functions/cliente.h
@@ -0,0 +1,11 @@
+#ifndef CLIENTE_H_INCLUDED
+#define CLIENTE_H_INCLUDED
+
+typedef struct{
+    int nCliente;
+    int nip;
+    char nombre[50];
+    float saldo;
+} cliente;
+
+#endif // CLIENTE_H_INCLUDED
